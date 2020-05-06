6277bd04090d775ad30f6d24049f37c5eb8da6fd
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 20:32:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,10 @@ end
 #   false 
 # end
 
+def long_planeteer_calls(array)
+  array[0].length > 4 ? return true : return long_planeteer_calls(array.shift)
+end
+
 def find_the_cheese(array) 
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |element|
