d6694c5832a254a061a312f1cfe138833d355204
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 20:31:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,12 +6,12 @@ def summon_captain_planet(array)
   array.map {|call| call.capitalize << "!"}
 end
 
-def long_planeteer_calls(array)
-  array.each do |element|
-    return true if element.length > 4
-  end
-  false 
-end
+# def long_planeteer_calls(array)
+#   array.each do |element|
+#     return true if element.length > 4
+#   end
+#   false 
+# end
 
 def find_the_cheese(array) 
   cheese_types = ["cheddar", "gouda", "camembert"]
